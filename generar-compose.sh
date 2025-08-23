f6c412cd7cf2e9c91c752f93c3e4bc378e9f83cc
Sebastian <svintonuke@fi.uba.ar>
2025-08-23 18:26:30 -0300

    ej1

---
+++ generar-compose.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+output_file_name=$1
+clients_number=$2
+
+if [ $# -ne 2 ] || ! [[ "$clients_number" =~ ^[0-9]+$ ]]; then
+    echo "$0 <Nombre del archivo de salida> <Cantidad de clientes>"
+    exit 1
+fi
+
+echo "Nombre del archivo de salida: $1"
+echo "Cantidad de clientes: $2"
+
+echo "name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+" > "$output_file_name"
+
+for ((i=1; i<=clients_number; i++)); do
+  echo "  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+" >> "$output_file_name"
+done
+
+echo "networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24" >> "$output_file_name"
